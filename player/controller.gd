2be1d68110f8f294316211f0216f319c4e39a866
Bastian Böhm <bastian.boehm-vhm@web.de>
2019-04-12 13:14:29 +0200

    Add type information

--- player/controller.gd
+++ player/controller.gd
@@ -14,19 +14,19 @@
 extends KinematicBody
 
 # TODO Centralized position
-const GRAVITY : = Vector3(0,-9.8,0) # TODO Use project setting
-const MOUSE_SPEED = 5.0
+const GRAVITY := Vector3(0,-9.8,0) # TODO Use project setting
+const MOUSE_SPEED := 5.0
 
-onready var camera : Camera = $Camera
+onready var camera := $Camera
 
-const SPEED_WALKING = 6.0
-const ACCELERATION_WALKING = 1.0
-const SPEED_RUNNING = SPEED_WALKING * 3.0
-const ACCELERATION_RUNNING = 1.0
+const SPEED_WALKING := 6.0
+const ACCELERATION_WALKING := 1.0
+const SPEED_RUNNING := SPEED_WALKING * 3.0
+const ACCELERATION_RUNNING := 1.0
 
-var velocity : = Vector3(0,0,0)
-var acceleration : = GRAVITY
-var jumping = false
+var velocity := Vector3(0,0,0)
+var acceleration := GRAVITY
+var jumping := false
 
 func _physics_process(delta):
 	if is_on_floor(): 
@@ -36,7 +36,7 @@ func _physics_process(delta):
 	move_and_slide(velocity, Vector3.UP)
 
 func calculate_movement_velocity() -> Vector3:
-	var velocity : = Vector3(0,0,0)
+	var velocity := Vector3(0,0,0)
 	
 	if Input.is_action_pressed("move_forward"):
 		velocity.z = -1
@@ -51,7 +51,7 @@ func calculate_movement_velocity() -> Vector3:
 	else:
 		velocity.x = 0
 	
-	var speed = SPEED_WALKING if not Input.is_action_pressed("run") else SPEED_RUNNING
+	var speed : float = SPEED_WALKING if not Input.is_action_pressed("run") else SPEED_RUNNING
 	velocity = velocity.normalized() * speed
 	velocity = velocity.rotated(Vector3.UP, rotation.y) # Match facing direction
 	
@@ -61,8 +61,8 @@ func _input(event : InputEvent):
 	if event is InputEventMouseMotion:
 		camera_movement(event)
 
-func camera_movement(event: InputEventMouseMotion):
-	var mod = MOUSE_SPEED * get_process_delta_time()
+func camera_movement(event : InputEventMouseMotion):
+	var mod := MOUSE_SPEED * get_process_delta_time()
 	
 	# Rotate body horizontally
 	rotate_y(-deg2rad(event.relative.x) * mod)
